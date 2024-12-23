deee6303d6d396d4d6a2884a14f6592e5eb49097
Nastia Yurash <nasstyayurash@gmail.com>
2024-12-23 14:48:20 +0200

    changes

--- main.js
+++ main.js
@@ -35,3 +35,30 @@ function startAnimation() {
 const observer = new IntersectionObserver(observeFunc, options);
 // Передаємо секцію в якості таргета для спостереження 
 observer.observe(coverSection)
+
+
+// faq js//
+// import Accordion from 'accordion-js';
+
+
+
+
+new Accordion('.faq-list', {
+  duration: 400,
+  showOne: true,
+  showMultiple: true,
+  onToggle: function (element) {
+    const parent = element.closest('.faq-item');
+    if (parent) {
+      parent.classList.toggle('is-active');
+      const panel = parent.querySelector('.ac-panel');
+      if (panel) {
+        if (parent.classList.contains('is-active')) {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        } else {
+          panel.style.maxHeight = null;
+        }
+      }
+    }
+  },
+});
